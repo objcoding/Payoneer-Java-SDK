d56c3753476c0c71dbb8fd1ec80fe142dee7be47
zch <zhangchenghui.dev@gmail.com>
2018-04-09 01:07:53 +0800

    no message

--- pay-center-demo/src/main/java/com/objcoding/pay/demo/AlipayController.java
+++
@@ -1,7 +0,0 @@
-package com.objcoding.pay.demo;
-
-/**
- * Created by chenghui.zhang on 2018/4/2.
- */
-public class AlipayController {
-}
--- wxpay/src/main/java/com/objcoding/wxpay/model/WXPayRefund.java
+++
@@ -1,7 +0,0 @@
-package com.objcoding.wxpay.model;
-
-/**
- * Created by chenghui.zhang on 2018/4/2.
- */
-public class WXPayRefund {
-}
